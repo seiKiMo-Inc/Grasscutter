9c1321d3c9892644d61f15b1676a4785b9b830c5
KingRainbow44 <kobedo11@gmail.com>
2022-09-21 21:49:20 -0400

    Implement basic logger + terminal

--- src/main/java/io/grasscutter/Grasscutter.java
+++ src/main/java/io/grasscutter/Grasscutter.java
@@ -1,10 +1,49 @@
 package io.grasscutter;
 
+import lombok.Getter;
+import lombok.SneakyThrows;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public final class Grasscutter {
+    @Getter private static final Logger logger =
+            LoggerFactory.getLogger(Grasscutter.class);
+    @Getter private static final LineReader console =
+            Grasscutter.createConsole();
+
     static {
         // Declare logback configuration.
         System.setProperty("logback.configurationFile", "src/main/resources/logback.xml");
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        logger.info("hello grasscutters");
+    }
+
+    /**
+     * Creates a {@link LineReader}, or "console" for the application.
+     * @return A {@link LineReader} instance.
+     *
+     * @throws RuntimeException if something impossible happened. (no dumb terminal created)
+     */
+    @SneakyThrows(IOException.class)
+    private static LineReader createConsole() {
+        Terminal terminal; try {
+            terminal = TerminalBuilder.builder()
+                    .jna(true).build();
+        } catch (IOException ignored) {
+            // Try to get a dumb terminal.
+            terminal = TerminalBuilder.builder()
+                    .dumb(true).build();
+        }
+
+        return LineReaderBuilder.builder()
+                .terminal(terminal).build();
+    }
 }
---
+++ src/main/java/io/grasscutter/utils/objects/JLineLogbackAppender.java
@@ -0,0 +1,22 @@
+package io.grasscutter.utils.objects;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.ConsoleAppender;
+import io.grasscutter.Grasscutter;
+
+import java.util.Arrays;
+
+/**
+ * A logback appender that uses JLine to print above the console.
+ * @see <a href="https://bit.ly/3S38YeL">Grasscutter/Grasscutters</a>
+ */
+public final class JLineLogbackAppender extends ConsoleAppender<ILoggingEvent> {
+    @Override protected void append(ILoggingEvent eventObject) {
+        if (!started) return;
+
+        Arrays.stream(
+                new String(encoder.encode(eventObject))
+                        .split(System.lineSeparator())
+        ).forEach(Grasscutter.getConsole()::printAbove);
+    }
+}
