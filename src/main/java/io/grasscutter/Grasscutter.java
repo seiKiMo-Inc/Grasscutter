c120f867b142d4cf3400cf1ff3aea1dc63c29259
KingRainbow44 <kobedo11@gmail.com>
2022-09-21 21:17:10 -0400

    Add `logback` to project

---
+++ src/main/java/io/grasscutter/Grasscutter.java
@@ -0,0 +1,10 @@
+package io.grasscutter;
+
+public final class Grasscutter {
+    static {
+        // Declare logback configuration.
+        System.setProperty("logback.configurationFile", "src/main/resources/logback.xml");
+    }
+
+    public static void main(String[] args) {}
+}
