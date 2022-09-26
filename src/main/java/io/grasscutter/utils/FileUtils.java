fedc652f37f2fcf97b88b2c6d4d707a432df23c2
KingRainbow44 <kobedo11@gmail.com>
2022-09-26 14:00:00 +0100

    Update utilities

--- src/main/java/io/grasscutter/utils/FileUtils.java
+++ src/main/java/io/grasscutter/utils/FileUtils.java
@@ -1,11 +1,18 @@
 package io.grasscutter.utils;
 
 import io.grasscutter.Grasscutter;
+import io.grasscutter.utils.interfaces.Serializable;
+import lombok.SneakyThrows;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Map;
 
 /* Utility methods for accessing the file system. */
 public interface FileUtils {
     /**
      * Reads a resource from the classpath.
+     *
      * @param path The path to the resource.
      * @return The resource as a byte array.
      */
@@ -17,5 +24,34 @@ public interface FileUtils {
         }
 
         return new byte[0];
+    }
+
+    /**
+     * Loads a file's data into an object.
+     *
+     * @param object A serializable object to load into.
+     * @param file The file to read the data from.
+     */
+    @SneakyThrows
+    static void loadFromFile(Serializable object, File file) {
+        // De-serialize data from the file.
+        Map<String, Object> data = EncodingUtils.fromJson(file, Serializable.SERIALIZE_TYPE);
+        // Load the data into the object.
+        object.deserialize(data);
+    }
+
+    /**
+     * Saves an object's data to a file.
+     *
+     * @param object A serializable object to save.
+     * @param file The file to save the data to.
+     */
+    @SneakyThrows
+    static void saveToFile(Serializable object, File file) {
+        // Serialize the object.
+        Map<String, Object> data = object.serialize();
+        var encoded = EncodingUtils.toJson(data);
+        // Write the data to the file.
+        Files.writeString(file.toPath(), encoded);
     }
 }
--- src/main/java/io/grasscutter/utils/NetworkUtils.java
+++ src/main/java/io/grasscutter/utils/NetworkUtils.java
@@ -4,27 +4,29 @@ import io.grasscutter.network.protocol.BasePacket;
 import io.grasscutter.network.protocol.Packet;
 import io.grasscutter.network.protocol.PacketIds;
 import io.grasscutter.utils.enums.KeyType;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
 
+import java.net.InetSocketAddress;
 import java.util.Arrays;
 
 /* Utility methods seen in server networking. */
 public interface NetworkUtils {
     /**
      * Identifies the packet ID for sending the packet.
+     *
      * @param packetClass The packet class.
      * @return A packet ID in {@link PacketIds}.
      */
     static int getSendIdOf(Class<? extends BasePacket<?, ?>> packetClass) {
         return Arrays.stream(Packet.values())
                 .filter(packet -> packet.getPacket() == packetClass)
-                .findFirst().orElse(Packet.NONE)
+                .findFirst()
+                .orElse(Packet.NONE)
                 .getSendId();
     }
 
     /**
      * Performs an XOR bitwise operation using the provided key.
+     *
      * @param buffer The buffer to encrypt.
      * @param keyType The key type to use.
      */
@@ -35,6 +37,7 @@ public interface NetworkUtils {
 
     /**
      * Performs an XOR bitwise operation on the given byte array.
+     *
      * @param buffer The buffer to XOR.
      * @param key The key to XOR with.
      */
@@ -45,22 +48,13 @@ public interface NetworkUtils {
     }
 
     /**
-     * Converts a Netty {@link ByteBuf} into a plain byte array.
-     * @param buffer The buffer to convert.
-     * @return The converted buffer.
+     * Creates an {@link InetSocketAddress} from the given host and port.
+     *
+     * @param address The address to parse.
+     * @param port The port to parse.
+     * @return The parsed address.
      */
-    static byte[] toByteArray(ByteBuf buffer) {
-        var bytes = new byte[buffer.readableBytes()];
-        buffer.readBytes(bytes); return bytes;
-    }
-
-    /**
-     * Converts a byte array into a Netty {@link ByteBuf}.
-     * @param bytes The bytes to convert.
-     * @return The converted bytes.
-     */
-    static ByteBuf toBuffer(byte[] bytes) {
-        var buffer = ByteBufAllocator.DEFAULT.buffer();
-        buffer.writeBytes(bytes); return buffer;
+    static InetSocketAddress createFrom(String address, int port) {
+        return new InetSocketAddress(address, port);
     }
 }
