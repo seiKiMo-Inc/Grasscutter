10df078cde58c85654818525ef6067899b35713b
KingRainbow44 <kobedo11@gmail.com>
2023-03-09 21:16:06 -0500

    Add a supported version constant
    This is the fallback for the game version during dispatch queries

---
+++ src/main/java/io/grasscutter/utils/constants/GameConstants.java
@@ -0,0 +1,7 @@
+package io.grasscutter.utils.constants;
+
+/* Constants seen in game-related handling. */
+public interface GameConstants {
+    // The current game version.
+    String GAME_VERSION = "3.3.0";
+}
