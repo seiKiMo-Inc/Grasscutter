0f7e4360a3a5dd0e53f6fab80c6e6f70a5f51c82
KingRainbow44 <kobedo11@gmail.com>
2023-03-11 22:02:55 -0500

    Update `WindSeedClient`

--- src/main/java/io/grasscutter/network/packets/WindSeedClient.java
+++ src/main/java/io/grasscutter/network/packets/WindSeedClient.java
@@ -1,19 +1,25 @@
 package io.grasscutter.network.packets;
 
 import com.google.protobuf.Empty;
+import io.grasscutter.network.NetworkSession;
 import io.grasscutter.network.protocol.BasePacket;
 import io.grasscutter.network.protocol.PacketIds;
-import io.grasscutter.proto.GetPlayerTokenRspOuterClass.GetPlayerTokenRsp;
 import io.grasscutter.proto.PacketHeadOuterClass.PacketHead;
+import io.grasscutter.proto.WindSeedClientNotifyOuterClass.WindSeedClientNotify;
 
 /** Wind seed client packet. {@link PacketIds#WindSeedClientNotify}. */
-public final class WindSeedClient extends BasePacket<Empty, GetPlayerTokenRsp> {
+public final class WindSeedClient extends BasePacket<Empty, WindSeedClientNotify> {
     public WindSeedClient() {
         // Empty constructor for handling.
     }
 
     @Override
-    public GetPlayerTokenRsp preparePacket() {
+    protected void handlePacket(NetworkSession session, PacketHead header, Empty message) {
+        super.handlePacket(session, header, message);
+    }
+
+    @Override
+    public WindSeedClientNotify preparePacket() {
         return super.preparePacket();
     }
 }
