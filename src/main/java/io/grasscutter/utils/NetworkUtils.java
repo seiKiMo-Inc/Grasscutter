cf76b86871428e3d289b5428c126f0c55e71a6a5
KingRainbow44 <kobedo11@gmail.com>
2023-03-11 21:01:17 -0500

    Cache packet IDs and names from the `PacketIds` class

--- src/main/java/io/grasscutter/utils/NetworkUtils.java
+++ src/main/java/io/grasscutter/utils/NetworkUtils.java
@@ -6,23 +6,76 @@ import io.grasscutter.network.protocol.PacketIds;
 import io.grasscutter.utils.definitions.Configuration;
 import io.grasscutter.utils.objects.Triplet;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 /* Utility methods seen in server networking. */
 public interface NetworkUtils {
+    Map<Integer, String> PACKET_NAMES = NetworkUtils.loadPacketNames();
+    Map<Class<? extends BasePacket<?, ?>>, Integer> PACKET_IDS = NetworkUtils.loadPacketIds();
+
+    /**
+     * Generates bindings from packet IDs -> packet names.
+     * @return A map of packet IDs -> packet names.
+     */
+    static Map<Integer, String> loadPacketNames() {
+        var map = new HashMap<Integer, String>();
+
+        // Read the packet IDs from the PacketIds class.
+        var constants = PacketIds.class.getFields();
+        for (var constant : constants) {
+            try {
+                // Read the value and the name.
+                var value = constant.getInt(null);
+                var name = constant.getName();
+                map.put(value, name);
+            } catch (IllegalAccessException ignored) { }
+        }
+
+        return map;
+    }
+
+    /**
+     * Generates bindings from packet classes -> packet IDs.
+     * @return A map of packet classes -> packet IDs.
+     */
+    static Map<Class<? extends BasePacket<?, ?>>, Integer> loadPacketIds() {
+        var map = new HashMap<Class<? extends BasePacket<?, ?>>, Integer>();
+
+        // Read the packet IDs from the PacketIds class.
+        var constants = PacketIds.class.getFields();
+        for (var constant : constants) {
+            try {
+                // Read the value and the name.
+                var value = constant.getInt(null);
+                var name = constant.getName();
+                var packet = Packet.valueOf(name).getPacket();
+                map.put(packet, value);
+            } catch (IllegalAccessException ignored) { }
+        }
+
+        return map;
+    }
+
     /**
      * Identifies the packet ID for sending the packet.
      *
      * @param packetClass The packet class.
      * @return A packet ID in {@link PacketIds}.
      */
-    static int getSendIdOf(Class<? extends BasePacket<?, ?>> packetClass) {
-        return Arrays.stream(Packet.values())
-                .filter(packet -> packet.getPacket() == packetClass)
-                .findFirst()
-                .orElse(Packet.NONE)
-                .getSendId();
+    static int getIdOf(Class<? extends BasePacket<?, ?>> packetClass) {
+        return PACKET_IDS.getOrDefault(packetClass, -1);
+    }
+
+    /**
+     * Identifies the packet name from a packet ID.
+     *
+     * @param packetId The packet ID.
+     * @return The packet name.
+     */
+    static String getNameOf(int packetId) {
+        return PACKET_NAMES.getOrDefault(packetId, "unknown");
     }
 
     /**
